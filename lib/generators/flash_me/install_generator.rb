2f7ec06a6dc6468f23fd24e4e2c4422381dc258b
Kevin J. Storberg <kevin@storberg.net>
2019-03-19 22:20:34 -0400

    troubleshooting

--- lib/generators/flash_me/install_generator.rb
+++ lib/generators/flash_me/install_generator.rb
@@ -1,6 +1,6 @@
 module FlashMe
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('templates', __dir__)
+    source_root File.expand_path('../templates', __FILE__)
 
     def copy_flash_me_concern_file
       # copy_file "flash_me_concern.rb", "app/controllers/concerns/flash_me.rb"
